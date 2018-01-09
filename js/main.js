6576621c07bfc0e8fa83a51b057a81fb5ed40a6e
brantchyoga <brantrporter@gmail.com>
2018-01-09 13:41:34 -0800

    crearted game logic

--- js/main.js
+++ js/main.js
@@ -1,21 +1,45 @@
 
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+{
+	suit : "Diamonds",
+	rank : "Queen",
+	cardImage : "images/queen-of-diamonds.png"
+},
+{
+	suit : "Hearts",
+	rank : "Queen",
+	cardImage : "images/queen-of-hearts.png"
+},
+{
+	suit : "Diamonds",
+	rank : "King",
+	cardImage : "images/king-of-diamonds.png"
+},
+{
+	suit : "Hearts",
+	rank : "King",
+	cardImage : "images/king-of-hearts.png"
+}
+];
+
+
 
 var cardsInPlay = [];
 
 var checkForMatch = function () {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
-		console.log("You found a match!");
+		alert("You found a match!");
 	} else {
-		console.log("sorry, try again.");
+		alert("sorry, try again.");
 	}
 };
 
 var flipCard = function (cardId) {
 
-	console.log("User flipped " + cards[cardId]);
-
-	cardsInPlay.push(cards[cardId]);
+	console.log("User flipped " + cards[cardId].rank);
+	console.log(cards[cardId].suit);
+	console.log(cards[cardId].cardImage);
+	cardsInPlay.push(cards[cardId].rank);
 
 	if (!(cardsInPlay.length === 2)) {
 	console.log(cardsInPlay.length);
