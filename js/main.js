8824d0c6ebbd8aedcb491924e850f93c6b57f520
brantchyoga <brantrporter@gmail.com>
2018-01-10 17:58:55 -0800

    created game logic

--- js/main.js
+++ js/main.js
@@ -3,44 +3,45 @@ var cards = [
 {
 	suit : "Diamonds",
 	rank : "Queen",
-	cardImage : "images/queen-of-diamonds.png"
+	cardImage : "img/queen-of-diamonds.png"
 },
 {
 	suit : "Hearts",
 	rank : "Queen",
-	cardImage : "images/queen-of-hearts.png"
+	cardImage : "img/queen-of-hearts.png"
 },
 {
 	suit : "Diamonds",
 	rank : "King",
-	cardImage : "images/king-of-diamonds.png"
+	cardImage : "img/king-of-diamonds.png"
 },
 {
 	suit : "Hearts",
 	rank : "King",
-	cardImage : "images/king-of-hearts.png"
+	cardImage : "img/king-of-hearts.png"
 }
-];
-
-
+]; 
 
 var cardsInPlay = [];
 
 var checkForMatch = function () {
+	console.log(cardsInPlay);
 	if (cardsInPlay[0] === cardsInPlay[1]) {
-		alert("You found a match!");
+		alert("You found a matching " + cardsInPlay[0] + "s!");
+		reset();
+		createBoard();
 	} else {
 		alert("sorry, try again.");
+		reset();
+		createBoard();
 	}
 };
 
-var flipCard = function (cardId) {
-
-	console.log("User flipped " + cards[cardId].rank);
-	console.log(cards[cardId].suit);
-	console.log(cards[cardId].cardImage);
+var flipCard = function () {
+	var cardId = this.getAttribute('data-id');
+	this.setAttribute('src', cards[cardId].cardImage);
 	cardsInPlay.push(cards[cardId].rank);
-
+	console.log(cardsInPlay);
 	if (!(cardsInPlay.length === 2)) {
 	console.log(cardsInPlay.length);
 	alert("Pick another card");
@@ -49,5 +50,26 @@ var flipCard = function (cardId) {
 	}
 };
 
-flipCard(0);
-flipCard(2);
+var board = document.getElementById('game-board');
+
+var createBoard = function(){
+	for(var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'img/back.png');
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		board.appendChild(cardElement);
+	};
+};
+
+var reset = function(){
+	board.innerHTML = '';
+	cardsInPlay = [];
+};
+
+createBoard();
+
+
+
+
+// Brant Porter
